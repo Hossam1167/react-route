c260b866c8650080b0a28adbeb470d70fbadd454
Hossam116 <hossam.baioumy@splintinvest.com>
2022-10-11 02:27:13 +0200

    React Router

---
+++ src/pages/Welcome.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Route } from "react-router-dom";
+
+const Welcome = () => {
+  return (
+    <>
+      <h1>Welcome</h1>
+      <Route path="/welcome/new-user">
+        <p>Welcome new user</p>
+      </Route>
+    </>
+  );
+};
+export default Welcome;
